7dfeeda2e1058c49733ab3c8f308da91ef50775c
mukhammadrizooff <legend.it@bk.ru>
2022-03-15 21:56:35 +0500

    Fix component error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
+import Header from './components/header';
 import Rockets from './pages/Rockets';
 import Missions from './pages/Missions';
 import Profile from './pages/Profile';
