37da11feec2c5d2324f5ccc8a2fb0d16e9065c48
mukhammadrizooff <legend.it@bk.ru>
2022-03-15 21:11:49 +0500

    Update header

---
+++ src/components/header.js
@@ -0,0 +1,52 @@
+import { NavLink } from 'react-router-dom';
+import planet from '../assets/planet.png';
+import './Header.scss';
+
+const Header = () => (
+  <header>
+    <NavLink
+      to="/"
+    >
+      <img className="logo-img" alt="logo" src={planet} />
+      <h1 className="title">Space Travelers&quot; Hub</h1>
+    </NavLink>
+    <nav className="header__nav">
+      <ul>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? 'active' : '')}
+            to="/"
+          >
+            Rockets
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? 'active' : '')}
+            to="/missions"
+          >
+            Missions
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? 'active' : '')}
+            to="/dragons"
+          >
+            Dragons
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? 'active' : '')}
+            to="/profile"
+          >
+            Profile
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Header;
