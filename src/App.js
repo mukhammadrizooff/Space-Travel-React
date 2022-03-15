3d347c1febf554a47ccdf7b86f90c58629685376
mukhammadrizooff <legend.it@bk.ru>
2022-03-16 04:15:32 +0500

    Fix required changes add linter errors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import Header from './components/Header';
 import Rockets from './pages/Rockets';
 import Missions from './pages/Missions';
 import Profile from './pages/Profile';
 import Dragons from './pages/Dragons';
-import elementStore from './redux/configureStore';
 
 const App = () => (
-  <Provider store={elementStore}>
+  <>
     <Header />
     <Routes>
       <Route exact path="/" element={<Rockets />} />
@@ -16,7 +14,7 @@ const App = () => (
       <Route path="/profile" element={<Profile />} />
       <Route path="/dragons" element={<Dragons />} />
     </Routes>
-  </Provider>
+  </>
 );
 
 export default App;
