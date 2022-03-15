5e241f6f6bafca7f2d2ca769fee70e85b0d64708
Sumair <sumairqaisar992@gmail.com>
2022-03-15 21:43:48 +0500

    fix linter error

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import React from 'react'
+import Header from '../components/Header'
+
+const Home = () => {
+  return (
+    <div>
+    <Header />
+    </div>
+  )
+}
+
+export default Home
