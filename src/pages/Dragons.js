c908f922309c95f4dbdd5370e69631bcc8ca5ad2
david-lafontant <lafontant.david@gmail.com>
2022-03-16 06:47:26 -0600

    Clean Dragons page

--- src/pages/Dragons.js
+++ src/pages/Dragons.js
@@ -1,32 +1,9 @@
-const URL = 'https://api.spacexdata.com/v3/dragons';
-
-const showAPI = (data) => {
-  console.log(data);
-};
-
-const getDragons = () => {
-  fetch(URL)
-    .then((resolve) => resolve.json())
-    .then((data) => {
-      showAPI(data);
-    });
-};
-
 const Dragons = () => (
   <>
     <h2>
       Dragons
 
     </h2>
-    <button
-      type="button"
-      onClick={() => {
-        getDragons();
-      }}
-      value="fetch"
-    >
-      Fetch
-    </button>
   </>
 );
 
