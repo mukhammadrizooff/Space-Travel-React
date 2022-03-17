60382bf58eaab7148e79501c7c5d72a1ea952827
Sumair <sumairqaisar992@gmail.com>
2022-03-18 00:29:52 +0500

    Render Missions Table

--- src/components/Missions/MissionsList.js
+++ src/components/Missions/MissionsList.js
@@ -1,23 +1,30 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import MissionsItem from './MissionsItem'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Table from 'react-bootstrap/Table';
+import MissionsItem from './MissionsItem';
 
 const MissionsList = () => {
-    const missions = useSelector((state)=> state.missions);
-    console.log(missions)
+  const missions = useSelector((state) => state.missions);
   return (
-    <ul>
-        
-        {missions.map((mission)=> (
-           <MissionsItem
-           name={mission.mission_name}
-           key={mission.mission_id}
-           description={mission.mission_description}
-           />
-
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {missions.map((mission) => (
+          <MissionsItem
+            name={mission.mission_name}
+            key={mission.mission_id}
+            description={mission.mission_description}
+          />
         ))}
-        </ul>
-  )
-}
+      </tbody>
+    </Table>
+  );
+};
 
-export default MissionsList
+export default MissionsList;
