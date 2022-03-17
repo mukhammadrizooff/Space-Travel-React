92d37768ff4dc373cfe8e6febb45360837e62865
david-lafontant <lafontant.david@gmail.com>
2022-03-16 19:24:31 -0600

    Update configureStore.js

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,16 +3,19 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import rocketsReducer, { fetchRocketsAPI } from './rockets/rockets';
-import missionsReducer, {fetchMissionsFromApi} from './missions/missions'
+import missionsReducer, {fetchMissionsFromApi} from './missions/missions';
+import dragonsReducer, {fetchDragonsAPI} from './dragons/dragons';
 
 const reducer = combineReducers({
+  dragonsReducer, 
   rocketsReducer,
   missions: missionsReducer
 });
 
 const elementStore = createStore(reducer, applyMiddleware(thunk, logger));
 elementStore.dispatch(fetchRocketsAPI());
- elementStore.dispatch(fetchMissionsFromApi());
+elementStore.dispatch(fetchMissionsFromApi());
+elementStore.dispatch(fetchDragonsAPI());
 elementStore.subscribe((state) => {
   console.log(state);
 });
