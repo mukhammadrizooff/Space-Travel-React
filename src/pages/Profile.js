09d3d499d539790f5fbd9aefd83f51400c4164ce
david-lafontant <lafontant.david@gmail.com>
2022-03-17 10:19:07 -0600

    Dispaly reserved dragon in Profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-const Profile = () => (
-  <div>Profile</div>
-);
+import { useSelector } from 'react-redux';
+
+const Profile = () => {
+  const dragonList = useSelector((state) => state.dragonsReducer);
+  const dragonReservation = dragonList.filter((dragon) => dragon.reservation === true);
+
+  return (
+    <div>
+      <h1>Profile</h1>
+      <div>
+        {
+            dragonReservation.map((item) => <p key={item.id} className="font-1">{item.name}</p>)
+          }
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
