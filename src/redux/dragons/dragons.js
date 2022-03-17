8e64629999a154b18e3c484219e272f3afcd2284
david-lafontant <lafontant.david@gmail.com>
2022-03-16 19:25:09 -0600

    Update Dragon reducer and actions

--- src/redux/dragons/dragons.js
+++ src/redux/dragons/dragons.js
@@ -1,34 +1,69 @@
-const URL = 'https://api.spacexdata.com/v3/dragons';
-
-export const displayFetchedDragons = (dragons) => ({
-  type: DISPLAY_DRAGONS, // eslint-disable-line
-  dragons,
-});
-
-const showDragons = (data) => {
-  console.log(data);
-};
-
-export const fetchDragons = () => (dispatch) => {
-  fetch(URL)
-    .then((resolve) => resolve.json())
-    .then((data) => {
-      dispatch(displayFetchedDragons(data));
-      showDragons(data);
-    })
-    .catch((error) => console.log(error));
-};
+const ADD_RESERVATION = 'space-travelers/dragons/ADD_dragon_RESERVATION';
+const REMOVE_RESERVATION = 'space-travelers/dragons/REMOVE__DRAGON_RESERVATION';
+const GET_DRAGONS = 'space-travelers/dragons/GET_DRAGONS';
+const baseURL = 'https://api.spacexdata.com/v3/dragons';
 
 const initialState = [];
 
-const dragonsReducer = (state = initialState, action = {}) => {
+export const addDragonReservation = (id) => ({
+  type: ADD_RESERVATION,
+  payload: {
+    id,
+  },
+});
+
+export const removeDragonReservation = (id) => ({
+  type: REMOVE_RESERVATION,
+  payload: {
+    id,
+  },
+});
+
+export const getdragons = (payload) => ({
+  type: GET_DRAGONS,
+  payload,
+});
+
+export const fetchDragonsAPI = () => async (dispatch) => {
+  await fetch(`${baseURL}`)
+    .then((response) => response.json())
+    .then((dragonsList) => {
+      const arrangedList = dragonsList.map((dragon) => ({
+        id: dragon.id,
+        name: dragon.name,
+        desc: dragon.description,
+        type: dragon.type,
+        flickr_images: dragon.flickr_images[0],
+        reservation: false,
+      }));
+      if (arrangedList) {
+        dispatch(getdragons(arrangedList));
+      }
+    });
+};
+
+// Function - Reducer
+const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case DISPLAY_DRAGONS: // eslint-disable-line
-      return [...state, ...action.dragons];
+    case ADD_RESERVATION: {
+      const newState = state.map((dragon) => {
+        if (dragon.id !== action.payload.id) return dragon;
+        return { ...dragon, reservation: true };
+      });
+      return [...newState];
+    }
+    case REMOVE_RESERVATION: {
+      const newState = state.map((dragon) => {
+        if (dragon.id !== action.payload.id) return dragon;
+        return { ...dragon, reservation: false };
+      });
+      return [...newState];
+    }
+    case GET_DRAGONS:
+      return [...action.payload];
     default:
-      console.log('Dispatched action has no matching case');
       return state;
   }
 };
 
-export default dragonsReducer;
+export default reducer;
