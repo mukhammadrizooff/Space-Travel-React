fb944fcbc36d357d26a0e7db001bb51dc3f57820
david-lafontant <lafontant.david@gmail.com>
2022-03-16 20:18:10 -0600

    Add margin to Dragons image

--- src/pages/Dragon.js
+++ src/pages/Dragon.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge';
 import { useDispatch } from 'react-redux';
 import { PropTypes } from 'prop-types';
 import { addDragonReservation, removeDragonReservation } from '../redux/dragons/dragons';
@@ -11,7 +12,7 @@ const Dragon = (props) => {
   const {
     id, name, image, desc, reservation, type,
   } = props;
-    // console.log(desc);
+  // console.log(desc);
   const handleReserveClick = () => {
     dispatch(addDragonReservation(id));
   };
@@ -22,17 +23,23 @@ const Dragon = (props) => {
 
   return (
     <Row id={id}>
-      <Col>
-        <img src={image} alt="rocket" />
+      <Col sm={4} lg={4}>
+        <img style={{ width: '100%', marginBottom: '50px' }} src={image} alt="rocket" />
       </Col>
-      <Col>
-        <h2>{name}</h2>
+      <Col sm={8} lg={8}>
+        <h2 style={{ display: 'flex', gap: '6rem' }}>
+          <span>
+            {name}
+          </span>
+          <span>
+            Type:
+            {type}
+          </span>
+
+        </h2>
+
         <p>
-          type of dragon
-          {type}
-        </p>
-        <p>
-          {reservation && <span>Reserved</span>}
+          {reservation && <Badge bg="warning" text="dark">Reserved</Badge>}
           {`${desc}`}
         </p>
         {reservation ? (
