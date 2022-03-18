a5e23387e6f93a844b336c831cfd0c076b44147e
mukhammadrizooff <legend.it@bk.ru>
2022-03-19 04:05:06 +0500

    Add missions test

---
+++ src/test/missions.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import elementStore from '../redux/configureStore';
+import Missions from '../pages/Missions';
+
+// Arangment
+const rockets = [
+  {
+    desc: 'The SpaceX Misions are best plan for system',
+    key: 'cheers',
+    id: 'csgo',
+    type: 'hard',
+    name: 'care',
+    image: 'url',
+    reservation: false,
+  },
+];
+
+describe('Test render the Missions Component', () => {
+  it('Missions section renders a hardcoded object, passed as a prop', () => {
+    // Action
+    const { container } = render(
+      <Provider store={elementStore}>
+        <Missions key={Missions[0]} dragon={Missions[0]} />
+      </Provider>,
+    );
+
+    // Assert
+    expect(container).toMatchSnapshot();
+  });
+});
