6ca231e4c953e4597e5ea1a90b5270c5b03176e6
david-lafontant <lafontant.david@gmail.com>
2022-03-18 19:14:53 -0600

    Fix bug

--- src/components/Rockets/elementRockets.js
+++ src/components/Rockets/elementRockets.js
@@ -1,6 +1,7 @@
 import { useDispatch } from 'react-redux';
 import Button from 'react-bootstrap/Button';
 import { PropTypes } from 'prop-types';
+import styles from '../../pages/Rockets.module.css';
 import { addRocketReservation, removeRocketReservation } from '../../redux/rockets/rockets';
 
 const elementRockets = (props) => {
@@ -18,20 +19,20 @@ const elementRockets = (props) => {
   };
 
   return (
-    <li className="rocket-container" id={id}>
+    <li className={styles.rocketContainer} id={id}>
       <img style={{ width: '350px' }} src={image} alt="rocket" />
-      <div className="rocket-description">
+      <div className={styles.rocketDescription}>
         <h2>{name}</h2>
         <p>
           {reserved && <span>Reserved</span>}
           { `${desc}` }
         </p>
         {reserved ? (
-          <Button type="button" className="remove-reservation" onClick={handleRemoveReserveClick}>
+          <Button type="button" className={styles.removeReservation} onClick={handleRemoveReserveClick}>
             Cancel Reservation
           </Button>
         ) : (
-          <Button type="button" variant="danger" className="add-reservation" onClick={handleReserveClick}>
+          <Button type="button" variant="danger" className={styles.addReservation} onClick={handleReserveClick}>
             Reserve Rocket
           </Button>
         )}
